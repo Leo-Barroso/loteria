113a1a5d21763dc25b3e30bd3fa47e1237358f19
Leo Barroso <leobarrosoadm@gmail.com>
2024-08-27 20:39:44 -0300

    ajustes na seleção de itens

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const selecionados = [];
 const maxNumbers = 15
-document.querySelectorAll("ul li").forEach((item) => {
+
+document.querySelectorAll("ul.items li").forEach((item) => {
   item.addEventListener("click", () => {
     const result = document.querySelector("#result"); 
-    item.classList.toggle("color-selecionada"); 
     const itemText = item.textContent;
     if (selecionados.includes(itemText)) {
+      item.classList.remove("color-selecionada"); 
       const index = selecionados.indexOf(itemText);
       if (index > -1) {
         selecionados.splice(index, 1);
       }
     } else {
-      selecionados.push(itemText);
-    }
-    if(selecionados.length > maxNumbers) {
-      alert("Somente é possível selecionar 15 números.")
-      return
-    }
+      if(selecionados.length < maxNumbers) {
+        item.classList.add("color-selecionada"); 
+        selecionados.push(itemText);
+        selecionados.sort()
+      } else {
+        item.classList.remove("color-selecionada"); 
+        alert("Somente é possível selecionar 15 números.")
+      }
+    } 
     result.textContent = selecionados.join(" - ");
   });
 });
-
